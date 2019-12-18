3f6662b1014bfd71e7571d4681551823d01280c9
Luis <luis.choque@whiz.pe>
2019-12-18 12:59:51 -0500

    added n

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 const accents = require('remove-accents');
+const latinize = require('latinize');
 
 function sanitaze(obj) {
     try {
         if (Array.isArray(obj)) {
             for (const key in obj) {
                 if (obj.hasOwnProperty(key)) {
-                    obj[key] = sanitaze(obj[key]);
+                    let element = obj[key];
+                    console.log(element);
+                    
+                    element = element.split('ñ').join('%PLACEHOLDER_N%');
+                    element = latinize(accents.remove(element));
+                    element = element.split('%PLACEHOLDER_N%').join('ñ');
+                    obj[key] = element;
                 }
             }
             return obj;
         }
 
-        if (typeof obj === 'string')
-            return accents.remove(obj);
-
+        if (typeof obj === 'string'){
+            let element = obj.split('ñ').join('%PLACEHOLDER_N%');
+            element = latinize(accents.remove(element));
+            element = element.split('%PLACEHOLDER_N%').join('ñ');
+            return element;
+        }
         if (obj instanceof Date)
             return obj;
 
@@ -23,11 +33,15 @@ function sanitaze(obj) {
         let result = {};
         for (const key in obj) {
             if (obj.hasOwnProperty(key)) {
-                const element = obj[key];
-                if (typeof element === 'string')
-                    result[accents.remove(key)] = accents.remove(element);
-                else
+                let element = obj[key];
+                if (typeof element === 'string'){
+                    element = element.split('ñ').join('%PLACEHOLDER_N%');
+                    element = latinize(accents.remove(element));
+                    element = element.split('%PLACEHOLDER_N%').join('ñ');
+                    result[accents.remove(key)] = element;
+                } else {
                     result[accents.remove(key)] = sanitaze(element);
+                }
             }
         }
         return result;  
