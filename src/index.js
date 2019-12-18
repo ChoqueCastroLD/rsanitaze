a989198dfe0d341c355db1b6dc9805223a857f06
Luis <luis.choque@whiz.pe>
2019-12-18 10:55:08 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const accents = require('remove-accents');
+
+function sanitaze(obj) {
+    try {
+        if (Array.isArray(obj)) {
+            for (const key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    obj[key] = sanitaze(obj[key]);
+                }
+            }
+            return obj;
+        }
+
+        if (typeof obj === 'string')
+            return accents.remove(obj);
+
+        if (obj instanceof Date)
+            return obj;
+
+        if (typeof obj !== 'object')
+            return obj;
+
+        let result = {};
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                const element = obj[key];
+                if (typeof element === 'string')
+                    result[accents.remove(key)] = accents.remove(element);
+                else
+                    result[accents.remove(key)] = sanitaze(element);
+            }
+        }
+    } catch (error) {
+        return obj;
+    }
+    return result;
+}
+
+module.exports = sanitaze;
